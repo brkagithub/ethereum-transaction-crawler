b12a6357b1f5c4fd8e03bc000fb528a0a9a504cc
brka <mbrkic16@gmail.com>
2023-02-21 18:57:47 +0100

    Add transaction fetching w/o infinite and paging

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,17 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import { z } from "zod";
+import CopyButton from "~/components/CopyButton";
 import Navbar from "~/components/Navbar";
-
+import { Transaction } from "~/server/api/routers/web3";
+import { ethers } from "ethers";
 import { api } from "~/utils/api";
 
-type Transaction = {
-  hash: string;
-  blockNumber: string;
-  timestamp: string;
-  from: string;
-  to: string;
-  value: string;
-  //txnfee/gas/?
-};
+type Transaction = z.infer<typeof Transaction>;
+
+let tableHeaders = ["Txn Hash", "Block", "Age", "From", "To", "Value"] as const;
 
 const Home: NextPage = () => {
-  const hello = api.web3.hello.useQuery({ text: "from tRPC" });
-
   // Takes a transaction object and renders it as a card
   const TransactionCard: React.FC<{ t: Transaction }> = ({ t }) => {
     return <div>transaction</div>;
@@ -26,7 +21,52 @@ const Home: NextPage = () => {
   const Transactions: React.FC<{ addressToFetchFrom: string }> = ({
     addressToFetchFrom,
   }) => {
-    return <div>transactions</div>;
+    // Fetch transactions with tRPC query from our router
+    const { data: transactionsData, isLoading } =
+      api.web3.transactions.useQuery({
+        address: addressToFetchFrom,
+      });
+
+    if (!transactionsData) return <div></div>;
+
+    let transactions = transactionsData.transactions;
+
+    return (
+      <table className="border-collapse bg-white shadow-lg">
+        <tr>
+          {tableHeaders.map((h) => (
+            <th className="border bg-blue-100 px-8 py-4 text-left">{h}</th>
+          ))}
+        </tr>
+        {transactions.map((t) => (
+          <tr>
+            <td className="border px-8 py-4">
+              <div className="flex cursor-pointer flex-row items-center justify-between ">
+                <span className="pr-2">{t.hash.substring(0, 15)}...</span>
+                <CopyButton text={t.hash}></CopyButton>
+              </div>
+            </td>
+            <td className="border px-8 py-4">{t.blockNumber}</td>
+            <td className="border px-8 py-4">{t.timeStamp}</td>
+            <td className="border px-8 py-4">
+              <div className="flex cursor-pointer flex-row items-center justify-between ">
+                <span className="pr-2">{t.from.substring(0, 10)}...</span>
+                <CopyButton text={t.from}></CopyButton>
+              </div>
+            </td>
+            <td className="cursor-pointer border px-8 py-4">
+              <div className="flex cursor-pointer flex-row items-center justify-between ">
+                <span className="pr-2">{t.to.substring(0, 10)}...</span>
+                <CopyButton text={t.to}></CopyButton>
+              </div>
+            </td>
+            <td className="border px-8 py-4">
+              {ethers.formatEther(t.value)} ETH
+            </td>
+          </tr>
+        ))}
+      </table>
+    );
   };
 
   return (
@@ -41,6 +81,8 @@ const Home: NextPage = () => {
       </Head>
       <div className="mx-auto flex max-w-7xl flex-col items-center px-2 pt-2 sm:px-6 lg:px-8">
         <Navbar></Navbar>
+        <div className="p-4"></div>
+        <Transactions addressToFetchFrom="0x459DDB554B6c85Cf1635216A271210D98c39a35C"></Transactions>
       </div>
     </>
   );
--- src/server/api/routers/web3.ts
+++ src/server/api/routers/web3.ts
@@ -1,6 +1,14 @@
 import { z } from "zod";
-
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const Transaction = z.object({
+  hash: z.string(),
+  blockNumber: z.string(),
+  timeStamp: z.string(),
+  from: z.string(),
+  to: z.string(),
+  value: z.string(),
+});
 
 export const web3Router = createTRPCRouter({
   hello: publicProcedure
@@ -8,6 +16,25 @@ export const web3Router = createTRPCRouter({
     .query(({ input }) => {
       return {
         greeting: `Hello ${input.text}`,
+      };
+    }),
+  transactions: publicProcedure
+    .input(z.object({ address: z.string() }))
+    .query(async ({ input }) => {
+      let apiUrl = `https://api.etherscan.io/api?module=account&action=txlist&address=${input.address}&startblock=0&endblock=99999999&page=1&offset=10&sort=desc&apikey=${process.env.ETHERSCAN_API_KEY}`;
+
+      let response = await fetch(apiUrl, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      let transactions: z.infer<typeof Transaction>[] = (await response.json())
+        .result;
+
+      return {
+        transactions,
       };
     }),
 });
